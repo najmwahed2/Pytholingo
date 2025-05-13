7d10ea29fd841306a0d5b5d71fadc50e95b60401
najmwahed2 <najmwahed@gmail.com>
2025-05-13 12:54:36 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,15 @@
+
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.02a5a29b88994423a1fc96069367cf2d',
+  appName: 'pytholingo-arabia-code',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  server: {
+    url: 'https://02a5a29b-8899-4423-a1fc-96069367cf2d.lovableproject.com?forceHideBadge=true',
+    cleartext: true
+  }
+};
+
+export default config;
